fbd23fb87c020df8817d44e725f25c9973a627cf
isbah <isbahmedferoz@gmail.com>
2023-05-04 20:52:00 +0500

    Introduction of repo

---
+++ intro.cpp
@@ -0,0 +1,11 @@
+//intro.cpp
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"This is the first repository in CPP.\nThis repository is for references and projects of C and C plus plus C++";
+	cout<<endl;
+
+	return 0;
+}
